30daf6d60fbba16c709d3fc2b3057dd83c6658a1
Jayson Reis <santosdosreis@gmail.com>
2019-03-12 18:41:08 +0100

    feat(delete_local_branches): Fetch before delete
    Signed-off-by: Jayson Reis <santosdosreis@gmail.com>

--- src/delete_local_branches.rs
+++ src/delete_local_branches.rs
@@ -1,15 +1,37 @@
+use env_logger::Builder;
 use git2::{BranchType, Repository};
+use log::{debug, error, info, LevelFilter};
+use std::process;
 
 fn main() -> Result<(), failure::Error> {
+    Builder::new().filter_level(LevelFilter::Info).try_init()?;
     let repository = Repository::discover(".")?;
+
+    fetch_new_data(&repository)?;
 
     for branch in repository.branches(Some(BranchType::Local))? {
         let (mut branch, _) = branch?;
         let branch_name = branch.name()?.unwrap_or("branch-name-not-utf8").to_owned();
         match branch.delete() {
-            Ok(()) => println!("Deleted {}", &branch_name),
-            Err(e) => println!("Error: {}", e.message()),
+            Ok(()) => info!("Deleted {}", &branch_name),
+            Err(e) => error!("{}", e.message()),
         }
     }
+    Ok(())
+}
+
+fn fetch_new_data(repository: &Repository) -> Result<(), failure::Error> {
+    // It is easier to call a subcommand than implement this with libgit :D
+    info!("Fetching repository data.");
+    let child = process::Command::new("git")
+        .current_dir(repository.path())
+        .args(&["fetch", "-p"])
+        .output()?;
+
+    debug!(
+        "Process return\nSTDOUT: {:?}\nSTDERR: {:?}",
+        String::from_utf8(child.stdout).unwrap(),
+        String::from_utf8(child.stderr).unwrap()
+    );
     Ok(())
 }
